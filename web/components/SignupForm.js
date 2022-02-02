5783a7b0e19123efe1edd1d4d87131ee7c2a7d3a
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2022-02-02 13:31:20 -0600

    Update Sign Up form to use Formik
    - created separate sign up form component for easier editing of the sign up page

---
+++ web/components/SignupForm.js
@@ -0,0 +1,119 @@
+import { Formik, Field } from 'formik';
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.username) {
+    errors.username = 'Please enter a Username.';
+  }
+
+  if (!values.email) {
+    errors.email = 'Please enter your email address.';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
+
+const TextInput = ({ name, formik }) => {
+  return (
+    <input
+      className={`text-black p-1 w-full ${
+        formik.errors[name] && formik.touched[name]
+          ? 'border-2 border-red-400'
+          : null
+      }`}
+      name={`${name}`}
+      id={`${name}`}
+      type="text"
+      autoComplete={`${name}`}
+      value={formik.values[name]}
+      {...formik.getFieldProps(`${name}`)}
+    />
+  );
+};
+
+export default function SignupForm({ players, survivors }) {
+  return (
+    <Formik
+      initialValues={{
+        username: '',
+        email: '',
+        picks: [],
+        mvp: '',
+      }}
+      validate={validate}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        console.log(values);
+        setSubmitting(false);
+        resetForm();
+      }}
+    >
+      {(formik) => (
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex flex-col items-center"
+        >
+          <div className="flex flex flex-col items-start mb-4 w-full">
+            <label htmlFor="name" className="mb-1">
+              Username:
+            </label>
+            <TextInput name="username" formik={formik} />
+
+            {formik.errors.username && formik.touched.username ? (
+              <div className="text-sm text-red-400">
+                {formik.errors.username}
+              </div>
+            ) : null}
+          </div>
+
+          <div className="flex flex-col items-start mb-4 w-full">
+            <label htmlFor="name" className="mb-1">
+              Email Address:
+            </label>
+            <TextInput name="email" formik={formik} />
+
+            {formik.errors.email && formik.touched.email ? (
+              <div className="text-sm text-red-400">{formik.errors.email}</div>
+            ) : null}
+          </div>
+          <fieldset>
+            <legend className="mb-2 text-left">
+              Select 5 Survivors for your tribe, and pick one to be your MVP
+              (who you think will win)
+            </legend>
+
+            {survivors.map((survivor) => (
+              <div className="flex" key={survivor.name}>
+                <Field
+                  type="radio"
+                  name="mvp"
+                  id={survivor.name}
+                  value={survivor.name}
+                  className="mr-2"
+                  onChange={formik.handleChange}
+                />
+
+                <Field
+                  type="checkbox"
+                  id={survivor.name}
+                  name="picks"
+                  value={survivor.name}
+                  className="mr-2"
+                  onChange={formik.handleChange}
+                />
+                {'  '}
+                <label htmlFor={survivor.name}>{survivor.name}</label>
+              </div>
+            ))}
+          </fieldset>
+
+          <button className="bg-gray-700 p-1 w-20 rounded" type="submit">
+            Sign Up
+          </button>
+        </form>
+      )}
+    </Formik>
+  );
+}
--- web/pages/signup.js
+++ web/pages/signup.js
@@ -2,13 +2,7 @@ import { survivors as survivorsList } from '../components/survivors';
 import { players as playersList } from '../components/players';
 import Head from 'next/head';
 import Layout from '../components/Layout';
-
-function Form() {
-  const registerUser = (event) => {
-    event.preventDefault(); // don't redirect the page
-    // where we'll add our form logic
-  };
-}
+import SignupForm from '../components/SignupForm';
 
 export default function picks({ players, survivors }) {
   return (
@@ -17,63 +11,7 @@ export default function picks({ players, survivors }) {
         <title>Survivor Fantasy Pool | Sign Up</title>
       </Head>
       <Layout>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            console.log('submitted');
-          }}
-          className="flex flex-col"
-        >
-          <div className="flex flex flex-col items-start mb-4">
-            <label htmlFor="name" className="mb-1">
-              Username:
-            </label>
-            <input
-              className="text-black p-1"
-              id="name"
-              type="text"
-              autoComplete="name"
-              required
-            />
-          </div>
-
-          <div className="flex flex-col items-start mb-4">
-            <label htmlFor="name" className="mb-1">
-              Email Address:
-            </label>
-            <input
-              className="text-black p-1"
-              id="email"
-              type="email"
-              autoComplete="email"
-              required
-            />
-          </div>
-          <fieldset>
-            <legend className="mb-2 text-left">
-              Select 5 Survivors for your tribe (you'll select your MVP in the
-              next step)
-            </legend>
-
-            {survivors.map((survivor) => (
-              <div className="flex">
-                <input
-                  type="checkbox"
-                  id={survivor.name}
-                  name="picks"
-                  value={survivor.name}
-                  className="mr-2"
-                />
-                {'  '}
-                <label htmlFor={survivor.name}>{survivor.name}</label>
-              </div>
-            ))}
-          </fieldset>
-
-          <button className="bg-gray-700 p-1 w-20 rounded" type="submit">
-            Sign Up
-          </button>
-        </form>
+        <SignupForm players={players} survivors={survivors} />
       </Layout>
     </div>
   );
