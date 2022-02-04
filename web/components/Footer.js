6c34bdbe18dfe2c425b1a51e1f7244078c9fdf72
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2022-02-04 14:52:08 -0600

    Change footer from h1 to p element

--- web/components/Footer.js
+++ web/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="flex items-center justify-center w-full py-2 text-sm">
-      <h1>Copyright © {new Date().getUTCFullYear()} Tanner Wiltshire</h1>
+      <p>Copyright © {new Date().getUTCFullYear()} Tanner Wiltshire</p>
     </div>
   );
 };
