a4e72e08b4d501d3eb4f2b2ea2094276f33bf042
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2022-03-18 10:24:51 -0500

    Change document id to replace spaces in the username with underscores.

--- web/pages/api/createplayer.js
+++ web/pages/api/createplayer.js
@@ -3,9 +3,10 @@ import Client from '../../components/Client';
 export default async function handler(req, res) {
   const { picks, username, email, mvp } = req.body;
   picks.splice(picks.indexOf(mvp), 1);
+  const id = username.replace(/ /g, '_');
   Client.create({
     _type: 'player',
-    _id: `${username}`,
+    _id: `${id}`,
     username: username,
     email: email,
     picks: picks.sort(),
