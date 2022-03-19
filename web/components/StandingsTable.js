b0cd6b6585e6d959b2087e1decccd503087212bd
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2022-03-19 15:34:13 -0500

    Update standings table view
    - Increase username column in width to 2/5 from 1/4 (usernames were overflowing)
    - Sort players by rank
    - Add alternating row colour for readability

--- web/components/StandingsTable.js
+++ web/components/StandingsTable.js
@@ -5,26 +5,27 @@ export default function StandingsTable({ episode, players }) {
       <table className="w-full">
         <thead className="bg-black flex text-white w-full">
           <tr className="flex w-full text-center md:text-left  items-center ">
-            <th className="p-2 text-left w-1/4">Name</th>
-            <th className="p-2 w-1/4">Rank</th>
-            <th className="p-2 w-1/4">Episode Score</th>
-            <th className="p-2 w-1/4">Total Score</th>
+            <th className="p-2 text-left w-2/5">Name</th>
+            <th className="p-2 w-1/5">Rank</th>
+            <th className="p-2 w-1/5">Episode Score</th>
+            <th className="p-2 w-1/5">Total Score</th>
           </tr>
         </thead>
 
         <tbody className="bg-grey-light flex flex-col items-center justify-between w-full">
           {players
             .filter((player) => player.paid)
+            .sort((a, b) => a.rank - b.rank)
             .map(({ username, rank, episodeScores, totalScore }, index) => {
               return (
                 <tr
                   key={index}
-                  className="flex w-full text-center md:text-left mb-1"
+                  className={`flex w-full text-center md:text-left mb-1 even:bg-black/10 dark:even:bg-white/10`}
                 >
-                  <td className="p-2 text-left w-1/4">{username}</td>
-                  <td className="p-2 w-1/4">{rank}</td>
-                  <td className="p-2 w-1/4">{episodeScores[episode - 2]}</td>
-                  <td className="p-2 w-1/4">{totalScore}</td>
+                  <td className="p-2 text-left w-2/5">{username}</td>
+                  <td className="p-2 w-1/5">{rank}</td>
+                  <td className="p-2 w-1/5">{episodeScores[episode - 2]}</td>
+                  <td className="p-2 w-1/5">{totalScore}</td>
                 </tr>
               );
             })}
