adb390e669dce44708c9d75e7f34cc6a623537e0
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2022-11-02 12:35:05 -0500

    Change HTTP status code, correct behaviour of API, returning from API only after all requests are complete.

--- web/pages/api/deletescores.js
+++ web/pages/api/deletescores.js
@@ -2,13 +2,32 @@ import Client from '../../components/Client';
 
 export default async function handler(req, res) {
   const { players } = req.body;
+  const updateRequests = [];
 
   players.forEach((player) => {
     const { _id } = player;
-    Client.patch(_id)
-      .set({ totalScore: 0, episodeScores: [], rank: [] })
-      .commit();
+    updateRequests.push(
+      Client.patch(_id)
+        .set({ totalScore: 0, episodeScores: [], rank: [] })
+        .commit()
+        .then((player) => {
+          console.log(`Successfully reset score for ${player.username}`);
+        })
+        .catch((err) => {
+          console.log(
+            `An error occurred while resetting scores for ${player.username}: ${err}`
+          );
+        })
+    );
   });
 
-  res.status(201).json(req.body);
+  try {
+    const result = await Promise.all(updateRequests);
+    res.status(200).send(`Scores successfully reset for all players.`);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ error: 'An error occurred in one or more update requests:' });
+    console.error(err);
+  }
 }
