3929daf7a4ecc55aa7caffa0aec66b11365ffdfd
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2022-11-02 12:24:12 -0500

    Change HTTP status code, error message

--- web/pages/api/deleteplayers.js
+++ web/pages/api/deleteplayers.js
@@ -3,9 +3,9 @@ import Client from '../../components/Client';
 export default async function handler(req, res) {
   Client.delete({ query: '*[_type == "player"]' })
     .then(() => {
-      res.status(201).json(req.body);
+      res.status(200);
     })
     .catch((err) => {
-      console.error(err);
+      console.log(`An error occurred with the request: ${err}`);
     });
 }
