0975906b7896ed81b68d7a54eb65355453685f0d
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2023-06-27 15:53:14 -0500

    Fix status and error sending.

--- web/pages/api/createplayer.js
+++ web/pages/api/createplayer.js
@@ -2,24 +2,27 @@ import Client from '../../components/Client';
 
 export default async function handler(req, res) {
   const { picks, username, email, mvp } = req.body;
-  picks.splice(picks.indexOf(mvp), 1);
+
   const id = username.replace(/ /g, '_');
-  Client.create({
+  const request = Client.create({
     _type: 'player',
     _id: `${id}`,
     username: username,
     email: email,
-    picks: picks.sort(),
+    picks: picks,
     mvp: mvp,
     episodeScores: [],
     totalScore: 0,
     rank: [],
     paid: false,
-  })
-    .then(() => {
-      res.status(201).json(req.body);
-    })
-    .catch((err) => {
-      console.error(err);
+  });
+
+  try {
+    const result = await request;
+    res.status(201).send(`Successfully created new player. Result: ${result}`);
+  } catch (err) {
+    res.status(500).send({
+      error: `An error occured while trying to create a new player: ${err}`,
     });
+  }
 }
