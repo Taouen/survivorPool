67fdc60ef9eabb0c39d090e043983ed7903a095e
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2023-07-24 15:36:50 -0500

    Add Login/Logout link, Admin dashboard link

--- web/components/Header.js
+++ web/components/Header.js
@@ -3,6 +3,8 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 
 const Header = () => {
   const { user, error, isLoading } = useUser();
+  const isAdmin =
+    user?.['https://www.tannerwiltshire.com/roles'].includes('admin');
   const linkStyles =
     'px-2 md:px-4 hover:text-red-500 dark:hover:text-red-400 hover:underline';
 
@@ -34,15 +36,18 @@ const Header = () => {
             <Link href={page.path}>{page.title}</Link>
           </li>
         ))}
-        {user ? (
-          <a className={linkStyles} href="/api/auth/logout">
-            Logout {user.name}{' '}
-          </a>
-        ) : (
-          <a className={linkStyles} href="/api/auth/login">
-            Login
-          </a>
-        )}
+        {isAdmin ? (
+          <li className={linkStyles}>
+            <Link href="/admin">Admin Dashboard</Link>
+          </li>
+        ) : null}
+        <li className={linkStyles}>
+          {user ? (
+            <a href="/api/auth/logout">Logout {user.name}</a>
+          ) : (
+            <a href="/api/auth/login">Login</a>
+          )}
+        </li>
       </ul>
     </div>
   );
