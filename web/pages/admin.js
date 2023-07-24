f3b5ce515c6e0e36175d29b94d6e4faa4ff5c1c4
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2023-07-24 15:37:00 -0500

    Add auth requirement to access page, admin role authorization requirement to access page content.

--- web/pages/admin.js
+++ web/pages/admin.js
@@ -2,7 +2,8 @@ import Head from 'next/head';
 import { useState } from 'react';
 import { Formik, Field } from 'formik';
 import { ClipLoader } from 'react-spinners';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 import Layout from '../components/Layout';
 import Client from '../components/Client.js';
@@ -175,7 +176,7 @@ const copyEmails = async () => {
   console.log(request.message);
 };
 
-export default withPageAuthRequired(function admin({ players, survivors }) {
+export default function admin({ error, players, survivors }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const initialValues = {
@@ -196,137 +197,153 @@ export default withPageAuthRequired(function admin({ players, survivors }) {
         <title>Survivor Fantasy Pool | Admin</title>
       </Head>
       <Layout>
-        <h2 className="mb-8 text-xl md:text-2xl">Welcome to the admin page.</h2>
-        <Formik
-          initialValues={initialValues}
-          validateOnBlur={false}
-          validateOnChange={false}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            updateScores(values, setIsSubmitting, players, survivors);
-            resetForm();
-          }}
-        >
-          {(formik) => (
-            /* --- FORM START --- */
-
-            <form
-              className="flex flex-col items-center mb-8 text-lg md:text-base"
-              onSubmit={formik.handleSubmit}
+        {error ? (
+          <div>{error}</div>
+        ) : (
+          <>
+            <h2 className="mb-8 text-xl md:text-2xl">
+              Welcome to the admin page.
+            </h2>
+            <Formik
+              initialValues={initialValues}
+              validateOnBlur={false}
+              validateOnChange={false}
+              onSubmit={(values, { setSubmitting, resetForm }) => {
+                updateScores(values, setIsSubmitting, players, survivors);
+                resetForm();
+              }}
             >
-              {survivors.map((survivor) => {
-                return (
-                  <div
-                    key={survivor.name}
-                    className="flex items-center justify-between w-full p-1"
-                  >
-                    <label htmlFor={survivor.name + ' Episode Score'}>
-                      {survivor.name}
-                    </label>
-                    <div>
-                      <Field
-                        type="number"
-                        name={`scores.${survivor.name}`}
-                        id={survivor.name + ' Episode Score'}
-                        onChange={formik.handleChange}
-                        aria-label={survivor.name}
-                        disabled={survivor.eliminated} // set disabled based on server data rather than the current formik value for eliminated.
-                        className="w-20 p-1 ml-4 text-black border rounded outline-none focus:ring focus:ring-lime-500"
-                      />
-                      <Field
-                        type="checkbox"
-                        id={survivor.name + ' Eliminated'}
-                        name="eliminated"
-                        value={survivor.name}
-                        className="w-6 h-6 ml-2 outline-none md:w-4 md:h-4 focus:ring focus:ring-lime-500"
-                        onChange={formik.handleChange}
-                        disabled={survivor.eliminated} // set disabled based on server data rather than the current formik value for eliminated.
-                        checked={formik.values.eliminated.includes(
-                          survivor.name
-                        )}
-                      />
+              {(formik) => (
+                /* --- FORM START --- */
+
+                <form
+                  className="flex flex-col items-center mb-8 text-lg md:text-base"
+                  onSubmit={formik.handleSubmit}
+                >
+                  {survivors.map((survivor) => {
+                    return (
+                      <div
+                        key={survivor.name}
+                        className="flex items-center justify-between w-full p-1"
+                      >
+                        <label htmlFor={survivor.name + ' Episode Score'}>
+                          {survivor.name}
+                        </label>
+                        <div>
+                          <Field
+                            type="number"
+                            name={`scores.${survivor.name}`}
+                            id={survivor.name + ' Episode Score'}
+                            onChange={formik.handleChange}
+                            aria-label={survivor.name}
+                            disabled={survivor.eliminated} // set disabled based on server data rather than the current formik value for eliminated.
+                            className="w-20 p-1 ml-4 text-black border rounded outline-none focus:ring focus:ring-lime-500"
+                          />
+                          <Field
+                            type="checkbox"
+                            id={survivor.name + ' Eliminated'}
+                            name="eliminated"
+                            value={survivor.name}
+                            className="w-6 h-6 ml-2 outline-none md:w-4 md:h-4 focus:ring focus:ring-lime-500"
+                            onChange={formik.handleChange}
+                            disabled={survivor.eliminated} // set disabled based on server data rather than the current formik value for eliminated.
+                            checked={formik.values.eliminated.includes(
+                              survivor.name
+                            )}
+                          />
+                        </div>
+                      </div>
+                    );
+                  })}
+                  {formik.errors.scores && formik.touched.scores ? (
+                    <div className="text-sm text-left text-red-400">
+                      {formik.errors.scores}
                     </div>
-                  </div>
-                );
-              })}
-              {formik.errors.scores && formik.touched.scores ? (
-                <div className="text-sm text-left text-red-400">
-                  {formik.errors.scores}
-                </div>
-              ) : null}
-              {formik.errors.eliminated && formik.touched.eliminated ? (
-                <div className="text-sm text-left text-red-400">
-                  {formik.errors.eliminated}
-                </div>
-              ) : null}
-              {isSubmitting && <ClipLoader color={'lime'} />}
-              <button
-                className="p-1 mt-4 border rounded"
-                onClick={formik.handleSubmit}
-                type="submit"
-              >
-                Update Scores
-              </button>
-            </form>
-          )}
-        </Formik>
+                  ) : null}
+                  {formik.errors.eliminated && formik.touched.eliminated ? (
+                    <div className="text-sm text-left text-red-400">
+                      {formik.errors.eliminated}
+                    </div>
+                  ) : null}
+                  {isSubmitting && <ClipLoader color={'lime'} />}
+                  <button
+                    className="p-1 mt-4 border rounded"
+                    onClick={formik.handleSubmit}
+                    type="submit"
+                  >
+                    Update Scores
+                  </button>
+                </form>
+              )}
+            </Formik>
+            {/* --- DANGER ZONE --- */}
+            <div className="w-full p-2 border border-red-500 rounded">
+              <h3 className="mb-4 text-xl ">Danger Zone</h3>
+              <div className="flex flex-col justify-center w-full md:flex-row">
+                <button
+                  className="p-1 mb-2 border rounded md:ml-2"
+                  onClick={() =>
+                    deleteLatestScore(players, survivors, setIsSubmitting)
+                  }
+                >
+                  Delete latest score
+                </button>
+                <button
+                  className="p-1 mb-2 border rounded md:ml-2"
+                  onClick={() => copyEmails(setIsSubmitting)}
+                >
+                  Copy Player Emails
+                </button>
 
-        {/* --- DANGER ZONE --- */}
-
-        <div className="w-full p-2 border border-red-500 rounded">
-          <h3 className="mb-4 text-xl ">Danger Zone</h3>
-          <div className="flex flex-col justify-center w-full md:flex-row">
-            <button
-              className="p-1 mb-2 border rounded md:ml-2"
-              onClick={() =>
-                deleteLatestScore(players, survivors, setIsSubmitting)
-              }
-            >
-              Delete latest score
-            </button>
-            <button
-              className="p-1 mb-2 border rounded md:ml-2"
-              onClick={() => copyEmails(setIsSubmitting)}
-            >
-              Copy Player Emails
-            </button>
-
-            <button
-              className="p-1 mb-2 border rounded md:ml-2"
-              onClick={() => clearEliminated(survivors, setIsSubmitting)}
-            >
-              Clear Eliminated
-            </button>
-            <button
-              className="p-1 mb-2 border rounded md:ml-2"
-              onClick={() => resetScores(players, survivors, setIsSubmitting)}
-            >
-              Reset scores
-            </button>
-            <button
-              className="p-1 mb-2 border rounded md:ml-2"
-              onClick={() => deletePlayers(setIsSubmitting)}
-            >
-              Delete all players
-            </button>
-          </div>
-        </div>
+                <button
+                  className="p-1 mb-2 border rounded md:ml-2"
+                  onClick={() => clearEliminated(survivors, setIsSubmitting)}
+                >
+                  Clear Eliminated
+                </button>
+                <button
+                  className="p-1 mb-2 border rounded md:ml-2"
+                  onClick={() =>
+                    resetScores(players, survivors, setIsSubmitting)
+                  }
+                >
+                  Reset scores
+                </button>
+                <button
+                  className="p-1 mb-2 border rounded md:ml-2"
+                  onClick={() => deletePlayers(setIsSubmitting)}
+                >
+                  Delete all players
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </Layout>
     </div>
   );
-});
-
-export async function getStaticProps() {
-  const survivors = await Client.fetch(
-    '*[_type == "survivor"] | order(name, asc)'
-  ).catch((err) => console.error(err));
-  const players = await Client.fetch(
-    '*[_type == "player"] | order(username asc) {..., mvp->{...}, picks[]->{...}}'
-  ).catch((err) => console.error(err));
-  return {
-    props: {
-      survivors,
-      players,
-    },
-    revalidate: 10,
-  };
 }
+
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps({ req, res }) {
+    const session = await getSession(req, res);
+    const survivors = await Client.fetch(
+      '*[_type == "survivor"] | order(name, asc)'
+    ).catch((err) => console.error(err));
+    const players = await Client.fetch(
+      '*[_type == "player"] | order(username asc) {..., mvp->{...}, picks[]->{...}}'
+    ).catch((err) => console.error(err));
+    if (
+      !session?.user['https://www.tannerwiltshire.com/roles'].includes('admin')
+    ) {
+      return { props: { error: 'Unauthorized', survivors, players } };
+    } else {
+      return {
+        props: {
+          survivors,
+          players,
+        },
+      };
+    }
+  },
+});
