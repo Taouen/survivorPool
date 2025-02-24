f7efcd9f12c89928f7527465c8859f6dae734a0b
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2025-02-24 12:27:08 -0600

    Updates for Season 48

--- web/components/Client.js
+++ web/components/Client.js
@@ -1,7 +1,7 @@
 const sanityClient = require('@sanity/client');
 const Client = sanityClient({
   projectId: '806pz8zb',
-  dataset: process.env.SANITY_DEVELOPMENT_DATASET || 'season47',
+  dataset: process.env.SANITY_DEVELOPMENT_DATASET || 'season48',
   apiVersion: '2022-02-08', // use current UTC date - see "specifying API version"!
   token: process.env.SANITY_TOKEN, // or leave blank for unauthenticated usage
   useCdn: false, // `false` if you want to ensure fresh data
--- web/pages/signup.js
+++ web/pages/signup.js
@@ -9,7 +9,7 @@ export default function signup({ survivors }) {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isError, setIsError] = useState(false);
   // https://currentmillis.com/?now use Milliseconds since Unix Epoch
-  const hideTime = 1727913600788; // Season 46 specific
+  const hideTime = 1741824000396; // Season 46 specific
   const hideSignup = Date.now() > hideTime;
 
   return (
