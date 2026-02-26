5f536e5752f39cd465c320b0a0e5830830e79fca
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2026-02-26 12:32:58 -0600

    Update for Season 50

--- web/components/Client.js
+++ web/components/Client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 const Client = createClient({
   projectId: '806pz8zb',
-  dataset: process.env.SANITY_DEVELOPMENT_DATASET || 'season49',
+  dataset: process.env.SANITY_DEVELOPMENT_DATASET || 'season50',
   apiVersion: '2022-02-08', // use current UTC date - see "specifying API version"!
   token: process.env.SANITY_TOKEN, // or leave blank for unauthenticated usage
   useCdn: false, // 'false' if you want to ensure fresh data
