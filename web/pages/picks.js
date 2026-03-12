097f898f56cc28b3b7a0382efdb9ab824c1e4c51
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2026-03-11 20:04:56 -0500

    Misspelled fuchsia

--- web/pages/picks.js
+++ web/pages/picks.js
@@ -22,7 +22,7 @@ const getTribeColorClassname = (color) => {
     case 'teal':
       return 'ring-3 ring-inset ring-teal-300';
     case 'fuchsia':
-      return 'ring-3 ring-inset ring-fucshia-500';
+      return 'ring-3 ring-inset ring-fuchsia-500';
     default:
       return '';
   }
@@ -40,85 +40,89 @@ export default function picks({ players }) {
             <p>Nobody has signed up yet, check back later!</p>
           )}
           {players.length > 0 &&
-            players.sort((a,b) => a.username.toLowerCase() < b.username.toLowerCase() ? -1 : 1).map(({ username, picks, mvp }) => {
-              return (
-                <table
-                  key={username}
-                  className="flex flex-col w-full mb-8 md:w-1/3 md:mx-2 md:mb-4 "
-                >
-                  <thead className="flex justify-center text-lg">
-                    <tr className="flex pb-2">
-                      <th>{username}</th>
-                    </tr>
-                  </thead>
-                  <tbody className="flex flex-col">
-                    <tr
-                      className={`flex justify-center p-2 border border-grey-500 ${
-                        mvp.tribeColor
-                          ? `${getTribeColorClassname(mvp.tribeColor)}`
-                          : ''
-                      }`}
-                    >
-                      <td
-                        className={`flex justify-center ${
-                          mvp.eliminated ? 'line-through opacity-50' : null
+            players
+              .sort((a, b) =>
+                a.username.toLowerCase() < b.username.toLowerCase() ? -1 : 1,
+              )
+              .map(({ username, picks, mvp }) => {
+                return (
+                  <table
+                    key={username}
+                    className="flex flex-col w-full mb-8 md:w-1/3 md:mx-2 md:mb-4 "
+                  >
+                    <thead className="flex justify-center text-lg">
+                      <tr className="flex pb-2">
+                        <th>{username}</th>
+                      </tr>
+                    </thead>
+                    <tbody className="flex flex-col">
+                      <tr
+                        className={`flex justify-center p-2 border border-grey-500 ${
+                          mvp.tribeColor
+                            ? `${getTribeColorClassname(mvp.tribeColor)}`
+                            : ''
                         }`}
                       >
-                        {mvp.eliminated && (
-                          <FontAwesomeIcon
-                            icon={faSkull}
-                            style={{ color: '#7f7f7f80' }}
-                            size="2x"
-                            className="absolute self-center text-black -z-1 justify-self-center"
-                          />
-                        )}
-                        <p className="relative z-0">
-                          MVP: {mvp.nickname ? mvp.nickname : mvp.name}
-                        </p>
-                      </td>
-                    </tr>
-                    <tr className="flex justify-around border border-t-0 border-grey-500">
-                      {picks
-                        .sort((a, b) => {
-                          const aName = a.nickname || a.name;
-                          const bName = b.nickname || b.name;
-                          return aName > bName ? 1 : -1;
-                        })
-                        .map((pick) => (
-                          <td
-                            key={pick.nickname ? pick.nickname : pick.name}
-                            className={`flex justify-center w-1/4 p-2 border-r border-grey-500 last:border-r-0 ${
-                              pick.tribeColor
-                                ? `${getTribeColorClassname(pick.tribeColor)}`
-                                : ''
-                            }`}
-                          >
-                            <div
-                              className={`flex justify-center ${
-                                pick.eliminated
-                                  ? 'line-through opacity-50'
-                                  : null
+                        <td
+                          className={`flex justify-center ${
+                            mvp.eliminated ? 'line-through opacity-50' : null
+                          }`}
+                        >
+                          {mvp.eliminated && (
+                            <FontAwesomeIcon
+                              icon={faSkull}
+                              style={{ color: '#7f7f7f80' }}
+                              size="2x"
+                              className="absolute self-center text-black -z-1 justify-self-center"
+                            />
+                          )}
+                          <p className="relative z-0">
+                            MVP: {mvp.nickname ? mvp.nickname : mvp.name}
+                          </p>
+                        </td>
+                      </tr>
+                      <tr className="flex justify-around border border-t-0 border-grey-500">
+                        {picks
+                          .sort((a, b) => {
+                            const aName = a.nickname || a.name;
+                            const bName = b.nickname || b.name;
+                            return aName > bName ? 1 : -1;
+                          })
+                          .map((pick) => (
+                            <td
+                              key={pick.nickname ? pick.nickname : pick.name}
+                              className={`flex justify-center w-1/4 p-2 border-r border-grey-500 last:border-r-0 ${
+                                pick.tribeColor
+                                  ? `${getTribeColorClassname(pick.tribeColor)}`
+                                  : ''
                               }`}
                             >
-                              {pick.eliminated && (
-                                <FontAwesomeIcon
-                                  icon={faSkull}
-                                  style={{ color: '#7f7f7f80' }}
-                                  size="2x"
-                                  className="absolute self-center text-black -z-1 justify-self-center"
-                                />
-                              )}
-                              <p className="relative z-0">
-                                {pick.nickname ? pick.nickname : pick.name}
-                              </p>
-                            </div>
-                          </td>
-                        ))}
-                    </tr>
-                  </tbody>
-                </table>
-              );
-            })}
+                              <div
+                                className={`flex justify-center ${
+                                  pick.eliminated
+                                    ? 'line-through opacity-50'
+                                    : null
+                                }`}
+                              >
+                                {pick.eliminated && (
+                                  <FontAwesomeIcon
+                                    icon={faSkull}
+                                    style={{ color: '#7f7f7f80' }}
+                                    size="2x"
+                                    className="absolute self-center text-black -z-1 justify-self-center"
+                                  />
+                                )}
+                                <p className="relative z-0">
+                                  {pick.nickname ? pick.nickname : pick.name}
+                                </p>
+                              </div>
+                            </td>
+                          ))}
+                      </tr>
+                    </tbody>
+                  </table>
+                );
+              })}
         </div>
       </Layout>
     </div>
@@ -127,7 +131,7 @@ export default function picks({ players }) {
 
 export async function getServerSideProps() {
   const players = await Client.fetch(
-    '*[_type == "player"] | order(username asc) {..., mvp->{...}, picks[]->{...}}'
+    '*[_type == "player"] | order(username asc) {..., mvp->{...}, picks[]->{...}}',
   ).catch((err) => console.error(err));
 
   return {
