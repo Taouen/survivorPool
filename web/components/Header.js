2e65f3e3b443117fe944e52537762a61096612bb
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2026-06-06 16:15:52 -0500

    Update Header to use DaisyUI

--- web/components/Header.js
+++ web/components/Header.js
@@ -1,8 +1,7 @@
 import Link from 'next/link';
 
 const Header = () => {
-  const linkStyles =
-    'px-2 md:px-4 hover:text-red-500 dark:hover:text-red-400 hover:underline';
+  const linkStyles = 'hover:underline';
 
   const pages = [
     {
@@ -28,9 +27,63 @@ const Header = () => {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-around w-full h-24 p-2 mb-4 md:flex-row">
-      <h1 className="text-2xl md:text-4xl">Survivor Fantasy Pool</h1>
-      <ul className="flex">
+    <div className="mb-4 shadow-sm navbar bg-base-100">
+      <h1 className="p-2 text-xl navbar-start md:text-2xl ">
+        Survivor Fantasy Pool
+      </h1>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {' '}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{' '}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="w-32 p-2 mt-3 shadow menu menu-md dropdown-content bg-base-100 rounded-box z-1"
+          >
+            {pages.map((page) => (
+              <li key={page.title} className={linkStyles}>
+                <Link href={page.path} legacyBehavior>
+                  {page.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="hidden navbar-end md:flex">
+        <ul className="px-1 w-max menu menu-horizontal md:menu-md lg:menu-lg">
+          {pages.map((page) => (
+            <li key={page.title} className={linkStyles}>
+              <Link href={page.path} legacyBehavior>
+                {page.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+/* 
+<div className="flex flex-col items-center justify-between w-full h-24 p-2 mb-4 lg:justify-around md:flex-row">
+      <h1 className="p-2 text-xl md:text-2xl">Survivor Fantasy Pool</h1>
+      <ul className="menu md:menu-horizontal md:menu-md lg:menu-lg">
         {pages.map((page) => (
           <li key={page.title} className={linkStyles}>
             <Link href={page.path} legacyBehavior>
@@ -40,7 +93,4 @@ const Header = () => {
         ))}
       </ul>
     </div>
-  );
-};
-
-export default Header;
+*/
