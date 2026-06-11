5cc5388eafa02cda264058937c3c570ad33890c3
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2026-06-11 13:39:15 -0500

    Change size of danger zone

--- web/components/ui/DangerZone.js
+++ web/components/ui/DangerZone.js
@@ -1,6 +1,6 @@
 const DangerZone = ({ children, title = 'Danger Zone' }) => {
   return (
-    <div className="w-full p-2 border card card-border border-error bg-base-100">
+    <div className="p-2 border  card card-border border-error bg-base-100">
       <h3 className="mb-4 text-xl">{title}</h3>
       <div className="flex justify-around w-full">{children}</div>
     </div>
