d93e520f43736e720c3fdc07d75125fdfe69c351
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2026-06-12 12:28:46 -0500

    Update styles to allow extendng colours without needing to duplicate all classes

--- web/components/AdminNav.js
+++ web/components/AdminNav.js
@@ -4,9 +4,9 @@ const AdminNav = ({ currentPage, setCurrentPage }) => {
   const pages = ['Update Scores', 'Manage Survivors', 'Manage Players'];
 
   return (
-    <div className="flex justify-around w-full max-w-lg py-4 mx-auto md:w-3/4">
+    <div className="flex justify-around w-full py-4 mx-auto md:w-3/4">
       {pages.map((page) => {
-        const buttonClasses = `btn ${page === currentPage ? 'btn-primary' : 'btn-neutral'}`;
+        const buttonClasses = `${page === currentPage ? 'btn-primary' : 'btn-neutral'}`;
         return (
           <Button
             className={buttonClasses}
--- web/components/ui/Button.js
+++ web/components/ui/Button.js
@@ -1,5 +1,5 @@
 export default function Button({ className, children, ...props }) {
-  const styles = className ? className : `btn btn-primary m-1 flex-1`;
+  const styles = `btn ${className ? className : 'btn-primary'} py-1 m-1 flex-1`;
 
   return (
     <button className={styles} {...props}>
